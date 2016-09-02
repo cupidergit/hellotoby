8c91ce2aea54ca890a4517b3014ae324ada0c34e
Emery <emery@iubang.com>
2016-09-02 13:50:18 +0800

    change gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -35,11 +35,19 @@ let projectNames = [
 ];
 let accounts = [
     {
+        userName: 'adam3413',
+        password: 'Woaibaobao1107',
+    },
+    {
         userName: 'eve3777',
         password: 'Woaibaobao1107',
     },
     {
         userName: 'adolph8015',
+        password: 'Woaibaobao1107',
+    },
+    {
+        userName: 'dave4334',
         password: 'Woaibaobao1107',
     }
 ];
@@ -48,25 +56,17 @@ gulp.task('commit', () => {
     child_process_1.spawnSync('git', ['commit', '-m', `"commit-${(new Date()).toLocaleString()}"`], { stdio: 'inherit' });
 });
 gulp.task('default', () => {
-    child_process_1.execSync('powershell.exe unblock-file .\\credman.ps1', { stdio: 'inherit', encoding: 'utf8' });
+    child_process_1.execSync('powershell.exe unblock-file .\\credman.ps1', { stdio: 'inherit' });
     for (let account of accounts) {
         let credResult = child_process_1.spawnSync('powershell.exe', ['.\\credman.ps1', '-AddCred', '-Target', '"git:https://git.coding.net"', '-User', `"${account.userName}"`, '-Pass', `"${account.password}"`], { stdio: 'ignore', encoding: 'utf8' });
-        console.log(credResult);
         if (credResult.status != 0) {
-            console.log('set credential fail: ', account);
+            console.log('set cred fail: ', account);
         }
         for (let project of projectNames) {
             let gitUrl = `https://git.coding.net/${account.userName}/${project}.git`;
             let remoteName = `${account.userName}-${project}`;
             child_process_1.spawnSync('git', ['remote', 'add', remoteName, gitUrl], { stdio: 'ignore' });
-            // spawn('git', ['push', remoteName, 'coding-pages'], { stdio: 'ignore' }).on('exit', (code, signal) => {
-            //     if (code == 0) {
-            // console.log(`${account.userName}.coding.me/${project}`);
-            //     } else {
-            //         console.log(`push fail for ${remoteName}`);
-            //     }
-            // });
-            let result = child_process_1.spawnSync('git', ['push', remoteName, 'coding-pages', '-f'], { stdio: 'ignore', encoding: 'utf8' });
+            let result = child_process_1.spawnSync('git', ['push', remoteName, 'coding-pages'], { stdio: 'ignore', encoding: 'utf8' });
             if (result.status == 0) {
                 console.log(`${account.userName}.coding.me/${project}`);
             }
--- gulpfile.ts
+++ gulpfile.ts
@@ -1,6 +1,7 @@
 import * as gulp from 'gulp';
 import {execSync, spawn, spawnSync} from 'child_process';
 
+
 let projectNames = [
     'apple',
     'pear',
@@ -36,14 +37,23 @@ let projectNames = [
 
 let accounts: Array<account> = [
     {
+        userName: 'adam3413',
+        password: 'Woaibaobao1107',
+    },
+    {
         userName: 'eve3777',
         password: 'Woaibaobao1107',
     },
     {
         userName: 'adolph8015',
         password: 'Woaibaobao1107',
+    },
+    {
+        userName: 'dave4334',
+        password: 'Woaibaobao1107',
     }
 ];
+
 
 gulp.task('commit', () => {
     spawnSync('git', ['add', '*.html'], { stdio: 'inherit' });
@@ -51,20 +61,20 @@ gulp.task('commit', () => {
 });
 
 gulp.task('default', () => {
-    execSync('powershell.exe unblock-file .\\credman.ps1', { stdio: 'inherit', encoding: 'utf8' });
+    execSync('powershell.exe unblock-file .\\credman.ps1', { stdio: 'inherit' });
 
     for (let account of accounts) {
         let credResult = spawnSync('powershell.exe', ['.\\credman.ps1', '-AddCred', '-Target', '"git:https://git.coding.net"', '-User', `"${account.userName}"`, '-Pass', `"${account.password}"`], { stdio: 'ignore', encoding: 'utf8' });
         if (credResult.status != 0) {
-            console.log('set credential fail: ', account);
+            console.log('set cred fail: ', account);
         }
         for (let project of projectNames) {
             let gitUrl = `https://git.coding.net/${account.userName}/${project}.git`;
             let remoteName = `${account.userName}-${project}`;
             spawnSync('git', ['remote', 'add', remoteName, gitUrl], { stdio: 'ignore' });
-            let result = spawnSync('git', ['push', remoteName, 'coding-pages', '-f'], { stdio: 'ignore', encoding: 'utf8' });
+            let result = spawnSync('git', ['push', remoteName, 'coding-pages'], { stdio: 'ignore', encoding: 'utf8' });
             if (result.status == 0) {
-                console.log(`${account.userName}.coding.me/${project}`);
+                console.log(`${account.userName}.coding.me/${project}`)
             } else {
                 console.log(`push fail for ${remoteName}`);
             }
